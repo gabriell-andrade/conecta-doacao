64e6655f238b90595941aa9b02994efecafdda92
Gabriel Andrade <gabriel_anme@hotmail.com>
2026-04-07 16:40:46 -0300

    feat: implementa cadastro de doadores

---
+++ app/models.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+DATABASE = "database.db"
+
+def get_connection():
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def create_tables():
+    conn = get_connection()
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS doadores (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            email TEXT,
+            cep TEXT
+        )
+    """)
+    conn.commit()
+    conn.close()
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,28 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect
+from .models import get_connection
 
 main = Blueprint('main', __name__)
 
 @main.route("/")
 def home():
     return "Backend funcionando"
+
+
+@main.route("/doadores", methods=["GET", "POST"])
+def cadastrar_doador():
+    if request.method == "POST":
+        nome = request.form["nome"]
+        email = request.form["email"]
+        cep = request.form["cep"]
+
+        conn = get_connection()
+        conn.execute(
+            "INSERT INTO doadores (nome, email, cep) VALUES (?, ?, ?)",
+            (nome, email, cep)
+        )
+        conn.commit()
+        conn.close()
+
+        return redirect("/doadores")
+
+    return render_template("doadores.html")
